8105d49aa0ba7023535f63c4b15f6027207b4519
telemob020-lab <telemob020@gmail.com>
2026-08-11 01:57:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -158,7 +158,6 @@ Bu bot, resmi web sitesine ve bölgenizde mevcut olan bilgilere erişim sağlar.
         "url": "https://example.com/fa",
     },
 }
-}
 
 # ==========================================
 # /start
